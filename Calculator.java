1dbf3d8b0f3bbf97b9a041f93670467656da47b3
prarthana1305 <prarthanarnayaka@gmail.com>
2025-02-10 13:15:01 +0530

    added evaluate method to process and compute the result

---
+++ Calculator.java
@@ -0,0 +1,51 @@
+import java.util.Stack;
+
+public class Calculator {
+    public int evaluate(String expression) {
+        Stack<Integer> values = new Stack<>();
+        Stack<Character> operators = new Stack<>();
+
+        for (int i = 0; i < expression.length(); i++) {
+            char ch = expression.charAt(i);
+
+
+            if (ch == ' ') continue;
+
+
+            if (Character.isDigit(ch)) {
+                int num = 0;
+                while (i < expression.length() && Character.isDigit(expression.charAt(i))) {
+                    num = num * 10 + (expression.charAt(i) - '0');
+                    i++;
+                }
+                i--;
+                values.push(num);
+            }
+
+            else if (ch == '(') {
+                operators.push(ch);
+            }
+
+            else if (ch == ')') {
+                while (!operators.isEmpty() && operators.peek() != '(') {
+                    values.push(applyOp(operators.pop(), values.pop(), values.pop()));
+                }
+                operators.pop();
+            }
+
+            else if (ch == '+' || ch == '-' || ch == '*' || ch == '/') {
+                while (!operators.isEmpty() && precedence(operators.peek()) >= precedence(ch)) {
+                    values.push(applyOp(operators.pop(), values.pop(), values.pop()));
+                }
+                operators.push(ch);
+            }
+        }
+
+
+        while (!operators.isEmpty()) {
+            values.push(applyOp(operators.pop(), values.pop(), values.pop()));
+        }
+
+        return values.pop();
+    }
+}
